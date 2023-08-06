36dab76f57116a7b68333bd2dacaf9f3c1ca3c67
cfarias3266 <135296334+cfarias3266@users.noreply.github.com>
2023-08-05 20:26:03 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Irving + Cristian" />
+  <HelloWorld msg="Irvin + Cristian" />
 </template>
 
 <style scoped>
